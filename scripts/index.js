3b50ea8844308f752544836217cbc36382cf1dd0
Ana Faria <68421082+af-code2build@users.noreply.github.com>
2022-05-21 15:29:43 +0100

    Initial Version

---
+++ scripts/index.js
@@ -0,0 +1,107 @@
+
+// All DOM elements
+const nav_newGroup = document.querySelector('#new_group_option');
+const nav_newContact = document.querySelector('#new_contact_option');
+const slidePanel_closeButton = document.querySelector('.slider_close_button');
+const nav_contactsList = document.querySelector('#contact_list');
+const humburger_button = document.querySelector('#hamburger_menu');
+const nav_menu = document.querySelector('.menu');
+const contacts_panel = document.querySelector('#contacts_panel');
+const chat_panel = document.querySelector('#chat_panel');
+
+//EVENTS - MENU BUTTONS ACTIONS
+const show_slidePanel = function(title) {
+
+    const slide_panel = document.querySelector(".slider_panel");
+    const slide_panel_title = document.querySelector(".slider_panel h1");
+    slide_panel_title.innerText = title;
+    slide_panel.classList.add("visible_slider");
+
+    if(window.innerWidth < 1040){
+        humburger_button.classList.toggle("active");
+        nav_menu.classList.toggle("active");
+    }
+
+};
+
+nav_newGroup.addEventListener("click", () => show_slidePanel("Add a new Group"));
+nav_newContact.addEventListener("click", () => show_slidePanel("Add a new Contact"));
+
+// Show contacts panel and hide chat panel if you click the "Contacts List" option
+nav_contactsList.addEventListener("click", function() {
+
+    contacts_panel.classList.toggle("show_element");
+    chat_panel.classList.toggle("hidden_element");
+    humburger_button.classList.toggle("active");
+    nav_menu.classList.toggle("active");  
+
+});
+
+// Show both contact and chat panels after the window is a certain size
+window.onresize = function(){
+
+    if(window.innerWidth > 1040){
+
+        const contacts_panel = document.querySelector('#contacts_panel');
+        const chat_panel = document.querySelector('#chat_panel');
+        contacts_panel.classList.remove("show_element");
+        chat_panel.classList.remove("hidden_element");
+
+    }
+};
+
+// Close slide panel
+slidePanel_closeButton.addEventListener("click", function() {
+
+    const slide_panel = document.querySelector(".slider_panel");
+    const slide_panel_input = document.querySelector("#slider_content_contactName");
+    slide_panel_input.value = "";
+    slide_panel.classList.remove("visible_slider");
+
+});
+
+
+//Humburger menu action
+humburger_button.addEventListener("click", () => {
+
+    humburger_button.classList.toggle("active");
+    nav_menu.classList.toggle("active");
+
+});
+
+
+//Scroll to the bottom - replace with infinit scrolling
+function scrollToBottom(element) {
+    element.scroll({ top: element.scrollHeight, behavior: 'smooth' });
+}
+
+scrollToBottom(document.querySelector("#chat_panel"));
+
+
+//Create a new contact
+const newContact = document.createElement("div");
+const profilePictureContainer = document.createElement("div");
+const profilePicture = document.createElement("img");
+const profilePictureLabel = document.createElement("label");
+
+//Add styles
+newContact.classList.add("contact");
+profilePictureContainer.classList.add("profile_picture_background");
+profilePicture.classList.add("profile_picture");
+profilePictureLabel.classList.add("profile_name");
+
+//Add Content
+profilePicture.src = "../resources/kitty.webp";
+profilePicture.alt = "Lewis Profile Image"
+profilePictureLabel.innerText = "Lewis Carroll"
+
+//Stack html elements
+newContact.append(profilePictureContainer, profilePictureLabel);
+profilePictureContainer.appendChild(profilePicture);
+
+contacts_panel.appendChild(newContact);
+
+
+
+
+
